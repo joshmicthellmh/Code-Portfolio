257d39d265e763882b7e560250d1f5207c279272
CSJ <joshmitchellmh@gmail.com>
2020-02-21 12:21:56 +0000

    Frist Commit

---
+++ C#/TimesTableGame/TimesaableGamer2/Form1.Designer.cs
@@ -0,0 +1,176 @@
+﻿namespace TimesaableGamer2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnStart = new System.Windows.Forms.Button();
+            this.lblNum1 = new System.Windows.Forms.Label();
+            this.lblNum2 = new System.Windows.Forms.Label();
+            this.txtInput = new System.Windows.Forms.TextBox();
+            this.btnCheck = new System.Windows.Forms.Button();
+            this.lblScore = new System.Windows.Forms.Label();
+            this.lbltTable = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lbllevel = new System.Windows.Forms.Label();
+            this.btnQuit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(621, 282);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(125, 44);
+            this.btnStart.TabIndex = 0;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.BtnStart_Click);
+            // 
+            // lblNum1
+            // 
+            this.lblNum1.AutoSize = true;
+            this.lblNum1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNum1.Location = new System.Drawing.Point(189, 180);
+            this.lblNum1.Name = "lblNum1";
+            this.lblNum1.Size = new System.Drawing.Size(29, 31);
+            this.lblNum1.TabIndex = 1;
+            this.lblNum1.Text = "0";
+            // 
+            // lblNum2
+            // 
+            this.lblNum2.AutoSize = true;
+            this.lblNum2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNum2.Location = new System.Drawing.Point(262, 180);
+            this.lblNum2.Name = "lblNum2";
+            this.lblNum2.Size = new System.Drawing.Size(29, 31);
+            this.lblNum2.TabIndex = 2;
+            this.lblNum2.Text = "0";
+            // 
+            // txtInput
+            // 
+            this.txtInput.Location = new System.Drawing.Point(333, 187);
+            this.txtInput.Name = "txtInput";
+            this.txtInput.Size = new System.Drawing.Size(100, 20);
+            this.txtInput.TabIndex = 3;
+            // 
+            // btnCheck
+            // 
+            this.btnCheck.Location = new System.Drawing.Point(459, 180);
+            this.btnCheck.Name = "btnCheck";
+            this.btnCheck.Size = new System.Drawing.Size(78, 33);
+            this.btnCheck.TabIndex = 4;
+            this.btnCheck.Text = "Check";
+            this.btnCheck.UseVisualStyleBackColor = true;
+            this.btnCheck.Click += new System.EventHandler(this.BtnCheck_Click);
+            // 
+            // lblScore
+            // 
+            this.lblScore.AutoSize = true;
+            this.lblScore.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblScore.Location = new System.Drawing.Point(45, 355);
+            this.lblScore.Name = "lblScore";
+            this.lblScore.Size = new System.Drawing.Size(104, 29);
+            this.lblScore.TabIndex = 5;
+            this.lblScore.Text = "SCORE:";
+            // 
+            // lbltTable
+            // 
+            this.lbltTable.AutoSize = true;
+            this.lbltTable.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbltTable.Location = new System.Drawing.Point(224, 180);
+            this.lbltTable.Name = "lbltTable";
+            this.lbltTable.Size = new System.Drawing.Size(32, 31);
+            this.lbltTable.TabIndex = 6;
+            this.lbltTable.Text = "X";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(297, 180);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(30, 31);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "=";
+            // 
+            // lbllevel
+            // 
+            this.lbllevel.AutoSize = true;
+            this.lbllevel.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbllevel.Location = new System.Drawing.Point(41, 64);
+            this.lbllevel.Name = "lbllevel";
+            this.lbllevel.Size = new System.Drawing.Size(134, 39);
+            this.lbllevel.TabIndex = 8;
+            this.lbllevel.Text = "LEVEL:";
+            // 
+            // btnQuit
+            // 
+            this.btnQuit.Location = new System.Drawing.Point(621, 342);
+            this.btnQuit.Name = "btnQuit";
+            this.btnQuit.Size = new System.Drawing.Size(125, 42);
+            this.btnQuit.TabIndex = 9;
+            this.btnQuit.Text = "Quit";
+            this.btnQuit.UseVisualStyleBackColor = true;
+            this.btnQuit.Click += new System.EventHandler(this.BtnQuit_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnQuit);
+            this.Controls.Add(this.lbllevel);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lbltTable);
+            this.Controls.Add(this.lblScore);
+            this.Controls.Add(this.btnCheck);
+            this.Controls.Add(this.txtInput);
+            this.Controls.Add(this.lblNum2);
+            this.Controls.Add(this.lblNum1);
+            this.Controls.Add(this.btnStart);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Label lblNum1;
+        private System.Windows.Forms.Label lblNum2;
+        private System.Windows.Forms.TextBox txtInput;
+        private System.Windows.Forms.Button btnCheck;
+        private System.Windows.Forms.Label lblScore;
+        private System.Windows.Forms.Label lbltTable;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lbllevel;
+        private System.Windows.Forms.Button btnQuit;
+    }
+}
+
---
+++ C#/TimesTableGame/TimesaableGamer2/Form1.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TimesaableGamer2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public int num1 = 0;
+        public int num2 = 0;
+        public int input = 0;
+        public int gameScore = 0;
+        public int gameLevel = 0;
+        public int level = 1;
+
+        // on start the sets random numbers for both numbers, based on the score
+        private void BtnStart_Click(object sender, EventArgs e)
+        {
+            StartGame();
+        }
+
+        //Check if the the corect arnser was reached
+        private void BtnCheck_Click(object sender, EventArgs e)
+        {
+            GameOpiraation();
+            Num2Random();
+        }
+        //Sets all of the paramiters for game
+        public void StartGame()
+        {
+            btnStart.Enabled = false;
+            lbllevel.Text = "LEVEL:" + level.ToString();
+            Num1Random();
+            Num2Random();
+        }
+        //Alows the user to exit the game
+        private void BtnQuit_Click(object sender, EventArgs e)
+        {
+            System.Windows.Forms.Application.Exit();
+        }
+        // Sets a random numberr for num2 
+        public void Num2Random()
+        {
+            Random rnd = new Random();
+            int num2 = rnd.Next(2, 13);
+            lblNum2.Text = num2.ToString();
+        }
+        //this function runs the games operation
+        public void GameOpiraation()
+        {
+            //Checkimg the input is correct.
+            num1 = Convert.ToInt32(lblNum1.Text);
+            num2 = Convert.ToInt32(lblNum2.Text);
+            int Ressult = num1 * num2;
+            input = Convert.ToInt32(txtInput.Text);
+            if (input == Ressult)
+            {
+                MessageBox.Show("Well Done");
+                gameScore++;
+                lblScore.Text = "SCORE:" + gameScore.ToString();
+            }
+            else if (input != num1 * num2)
+            {
+                MessageBox.Show("Wrong!");
+            }
+            txtInput.Clear();
+            //Changing the level of the game. 
+            if (gameScore < 10 && gameScore >= 0)
+            {
+                level = 1;
+                Num1Random();
+                lbllevel.Text = "LEVEL:" + level.ToString();
+            }
+            else if (gameScore >= 10 && gameScore < 15)
+            {
+                level = 2;
+                Num1Random();
+                lbllevel.Text = "LEVEL:" + level.ToString();
+            }
+            else if (gameScore >= 15)
+            {
+                level = 3;
+                Num1Random();
+                lbllevel.Text = "LEVEL:" + level.ToString();
+            }
+        }
+        //controls the num 1 randomnas for all levels. 
+        public void Num1Random()
+        {    
+            //Takes the eusers current level and creares randfom value for num 1
+            Random rndl2 = new Random();
+            int counter = 0;
+            if (level == 1)
+            {
+                counter = rndl2.Next(1, 6);
+            }
+            else if (level == 2)
+            {
+                counter = rndl2.Next(1, 9);
+            }
+            else if (level == 3)
+            {
+                counter = rndl2.Next(1, 12);
+            }
+            switch (counter)
+            {
+                //Start of level 1
+                case 1:
+                    num1 = 2;
+                    lblNum1.Text = num1.ToString();
+                    break;
+                case 2:
+                    num1 = 3;
+                    lblNum1.Text = num1.ToString();
+                    break;
+                case 3:
+                    num1 = 4;
+                    lblNum1.Text = num1.ToString();
+                    break;
+                case 4:
+                    num1 = 5;
+                    lblNum1.Text = num1.ToString();
+                    break;
+                case 5:
+                    num1 = 10;
+                    lblNum1.Text = num1.ToString();
+                    break;
+                //End of level one 
+                //Start of level 2
+                case 6:
+                    num1 = 6;
+                    lblNum1.Text = num1.ToString();
+                    break;
+                case 7:
+                    num1 = 8;
+                    lblNum1.Text = num1.ToString();
+                    break;
+                case 8:
+                    num1 = 9;
+                    lblNum1.Text = num1.ToString();
+                    break;
+                //End of level 2 
+                //start of level 3
+                case 9:
+                    num1 = 7;
+                    lblNum1.Text = num1.ToString();
+                    break;
+                case 10:
+                    num1 = 11;
+                    lblNum1.Text = num1.ToString();
+                    break;
+                case 11:
+                    num1 = 12;
+                    lblNum1.Text = num1.ToString();
+                    break;
+                //End of level 3
+                //Defult Case
+                default:
+                    MessageBox.Show("Erro");
+                    break;
+            }
+
+        }
+    }
+}
